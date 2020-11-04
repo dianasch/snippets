0e4c6969ceeea6f0e8e0a30abd78c7f0f8a4c21e
dianasch <dianaschao@gmail.com>
2020-11-04 17:32:44 +0000

    Adds Artist class.

--- model.py
+++ model.py
@@ -18,8 +18,9 @@ def connect_to_db(flask_app, db_uri='postgresql:///snippet'), echo=True):
 
     print('Connected to the db!')
 
+
 class User(db.Model):
-    """A user, subclass of db.Model."""
+    """A user. Subclass of db.Model."""
 
     # Set table name as `users` for User objects
     __table__name = "users"
@@ -31,17 +32,41 @@ class User(db.Model):
                         autoincrement = True)
 
     # Create table column for email as a string
-    # Max limit of 100 characters, required field, must be unique
+    # Max limit of 100 chars, required field, must be unique
     email = db.Column(db.String(100),
                     nullable = False,
                     unique = True)
 
     # Create table column for password as a string
-    # Max limit of 20 characters, required field
+    # Max limit of 20 chars, required field
     password = db.Column(db.String(20),
                         nullable = False)
 
     # Method to identify each User instance
     def __repr__(self):
 
-        return f"User user_id={self.human_id} email={self.email}"
+        return f"<User user_id={self.human_id} email={self.email}>"
+
+
+class Artist(db.Model):
+    """A music artist. Subclass of db.Model."""
+
+    # Set table name as `artists` for Artist objects
+    __tablename__ = "artists"
+
+    # Create table column for artist_id as an integer
+    # This is the primary key for artists table
+    artist_id = db.Column(db.Integer,
+                        primary_key = True,
+                        autoincrement = True)
+
+    # Create table column for artist name as a string
+    # Max limit of 100 chars, required field
+    artist_name = db.Column(db.String(100),
+                            nullable = False)
+    
+    # Method to identify each Artist instance
+    def __repr__(self):
+
+        return f"""<Artist user_id={self.artist_id}
+                    artist_name={self.artist_name}>"""

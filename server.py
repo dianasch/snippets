339e4b52689f0eff0228bbfbb4f856c9ac3da17c
dianasch <dianaschao@gmail.com>
2020-11-04 20:42:52 +0000

    Initial commit.

---
+++ server.py
@@ -0,0 +1,12 @@
+"""Server for movie ratings app."""
+
+from flask import (Flask, render_template, request, flash, session,
+                    redirect)
+from model import connect_to_db
+
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")

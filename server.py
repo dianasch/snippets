e71d4171741d7a551e62822e6772cbfa0b3d227c
dianasch <dianaschao@gmail.com>
2020-11-06 21:34:02 +0000

    Adds following routes: /, /albums, /users, /login.

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ from model import connect_to_db
 import crud
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
@@ -20,7 +21,38 @@ def get_homepage():
 
 @app.route('/albums')
 
+@app.route('/users', methods = ['POST'] )
+def register_user():
+    """Get inputs from form"""
 
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = crud.get_user_by_email(email)
+
+    if user == None:
+        crud.create_user(email, password)
+        flash('Account created! You can now log in.')
+    else:    
+        flash('Email already exists. Try again.')
+
+    return redirect('/')
+
+@app.route('/login', methods = ['POST'])
+def log_in():
+    """Gets input from log-in and checks to see if emails and passwords
+    match."""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = crud.get_user_by_email(email)
+
+    if email == user.email and password == user.password:
+        session['user'] = user.user_id
+        flash('Logged in!')
+    else:
+        flash('Email and password do not match.')
+    
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")

b3f3687ba3666b82db530e8f7a612985b9a9d748
dianasch <dianaschao@gmail.com>
2020-11-13 19:23:44 +0000

    Adds jQuery to show snippet on album_details page.

--- static/js/snippet.js
+++ static/js/snippet.js
@@ -1,6 +1,30 @@
 "use strict";
+// alert('This is my js code')
 
+$(window).on('load', () => {
+    $('#create-snippet').on('click', (evt) => {
+        evt.preventDefault();
 
+        $.ajax({
+
+            url: "/albums/1/snippet",
+
+            type: "GET",
+
+            dataType: 'text',
+
+            success: (res) => {
+                console.log('loaded', res);
+                $('#display-snippet').html(res);
+            }
+        });
+
+    });
+});
+
+// $.get('/albums/1/snippet', (res) => {
+//     $('#display-snippet').html(res);
+// });
 
 
 // $('#create-snippet').on('click', (evt) => {

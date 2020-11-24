4d55ec34d9a1bfdf4dfc8a77d496552c43cba8d1
dianasch <dianaschao@gmail.com>
2020-11-24 22:08:55 +0000

    Adds closing tag for logout form.

--- test_server.py
+++ test_server.py
@@ -1,5 +1,6 @@
 import unittest
 from server import app
+from model import connect_to_db
 
 class FlaskTests(unittest.TestCase):
     """Test routes for snippet app."""
@@ -13,6 +14,12 @@ class FlaskTests(unittest.TestCase):
 
         result = self.client.get('/')
         self.assertIn(b'make an original Taylor Swift song snippet', result.data)
+
+    # def test_albums_page(self):
+    #     """Test albums route."""
+
+    #     result = self.client.get('/albums')
+    #     self.assertIn(b'Taylor Swift album', result.data)
 
 
 if __name__ == '__main__':
